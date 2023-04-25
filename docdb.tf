62f02d03250d1e12f59f4ad623beb8898f18f746
amankumar1988 <kumar.aman6600@gmail.com>
2023-04-25 23:05:00 +0530

    Auto Push

--- docdb.tf
+++ docdb.tf
@@ -1,20 +1,23 @@
-resource "aws_docdb_cluster" "docdb" {
-  cluster_identifier      = "robot-${var.ENV}-docdb"
-  engine                  = "docdb"
-  master_username         = "admin1"
-  master_password         = "roboshop1"
-  # backup_retention_period = 5       // Commenting this in lab we don't need it.
-  # preferred_backup_window = "07:00-09:00"
-  skip_final_snapshot     = true
-}
+# resource "aws_docdb_cluster" "docdb" {
+#   cluster_identifier      = "robot-${var.ENV}-docdb"
+#   engine                  = "docdb"
+#   master_username         = "admin1"
+#   master_password         = "roboshop1"
+#   # backup_retention_period = 5       // Commenting this in lab we don't need it.
+#   # preferred_backup_window = "07:00-09:00"
+#   skip_final_snapshot     = true
+# }
 
 # Create Subnet Group needed to host the docdb cluster
 
-resource "aws_docdb_subnet_group" "docdb_sgbnet_group[]" {
+resource "aws_docdb_subnet_group" "docdb_sgbnet_group" {
   name       = "robot-${var.ENV}-docdb_sgbnet_group"
-  subnet_ids = [aws_subnet.frontend.id, aws_subnet.backend.id]
-
+  subnet_ids = data.terraform_remote_state.vpc.PRIVATE_SUBNET_IDS
   tags = {
-    Name = "My docdb subnet group"
+    Name = "robot-${var.ENV}-docdb-subnet-group"
   }
+}
+
+output "output_ref"{
+  value = data.terraform_remote_state.vpc
 }
