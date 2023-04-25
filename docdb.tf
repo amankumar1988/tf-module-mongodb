c68621be1f27c14e8c1a976de486d5a4467ab9fe
amankumar1988 <kumar.aman6600@gmail.com>
2023-04-25 23:11:08 +0530

    Auto Push

--- docdb.tf
+++ docdb.tf
@@ -1,12 +1,13 @@
-# resource "aws_docdb_cluster" "docdb" {
-#   cluster_identifier      = "robot-${var.ENV}-docdb"
-#   engine                  = "docdb"
-#   master_username         = "admin1"
-#   master_password         = "roboshop1"
-#   # backup_retention_period = 5       // Commenting this in lab we don't need it.
-#   # preferred_backup_window = "07:00-09:00"
-#   skip_final_snapshot     = true
-# }
+resource "aws_docdb_cluster" "docdb" {
+  cluster_identifier      = "robot-${var.ENV}-docdb"
+  engine                  = "docdb"
+  master_username         = "admin1"
+  master_password         = "roboshop1"
+  # backup_retention_period = 5       // Commenting this in lab we don't need it.
+  # preferred_backup_window = "07:00-09:00"
+  skip_final_snapshot     = true
+  db_subnet_group_name    =  aws_docdb_subnet_group.docdb_sgbnet_group.name
+}
 
 # Create Subnet Group needed to host the docdb cluster
 
