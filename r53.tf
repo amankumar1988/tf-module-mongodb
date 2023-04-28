f0cd21115c919a4bc49ba83ae74903b79518ecf6
amankumar1988 <kumar.aman6600@gmail.com>
2023-04-28 22:13:21 +0530

    Auto Push

--- r53.tf
+++ r53.tf
@@ -1,9 +1,12 @@
 # This creates CNAME record for mongodb
 
 resource "aws_route53_record" "www" {
-  zone_id = "Z03162902ZBUWVJLMZQNI"
-  name    = "www.example.com"
-  type    = "A"
-  ttl     = 300
-  records = [aws_eip.lb.public_ip]
+  zone_id = data.terraform_remote_state.vpc.outputs.PRIVATE_HOSTED_ZONE_ID
+  name    = "mongodb-${var.ENV}-data.terraform_remote_state.vpc.outputs.PRIVATE_HOSTED_ZONE_NAME"
+  type    = "CNAME"
+  ttl     = 10
+  records = [aws_docdb_cluster.docdb.endpoint]
 }
+
+# A record : Creating name to an IP add
+# C record : Creating name to name
