7daa9fde10871d955f133e6bb76eb89974500b82
amankumar1988 <kumar.aman6600@gmail.com>
2023-04-28 21:10:23 +0530

    Auto Push

--- schema.tf
+++ schema.tf
@@ -7,8 +7,8 @@ resource "null_resource" "schema" {
         unzip -o mongodb.zip
         cd mongodb-main
         wget https://s3.amazonaws.com/rds-downloads/rds-combined-ca-bundle.pem
-        mongo --ssl --host ${aws_docdb_cluster.docdb.endpoint} --sslCAFile rds-combined-ca-bundle.pem --username admin1 --password Roboshop1 < catalogue.js
-        mongo --ssl --host ${aws_docdb_cluster.docdb.endpoint} --sslCAFile rds-combined-ca-bundle.pem --username admin1 --password Roboshop1 <  users.js
+        mongo --ssl --host ${aws_docdb_cluster.docdb.endpoint} --sslCAFile rds-combined-ca-bundle.pem --username admin1 --password roboshop1 < catalogue.js
+        mongo --ssl --host ${aws_docdb_cluster.docdb.endpoint} --sslCAFile rds-combined-ca-bundle.pem --username admin1 --password roboshop1 <  users.js
         EOF
     }
 }
