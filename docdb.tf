acea03bc8475d2b8c5a18a233c23fce4aafe8f03
amankumar1988 <kumar.aman6600@gmail.com>
2023-04-28 19:15:24 +0530

    Auto Push

--- docdb.tf
+++ docdb.tf
@@ -6,7 +6,7 @@ resource "aws_docdb_cluster" "docdb" {
   # backup_retention_period = 5       // Commenting this in lab we don't need it.
   # preferred_backup_window = "07:00-09:00"
   skip_final_snapshot     =   true
-  db_subnet_group_name    =  aws_docdb_subnet_group.docdb_sgbnet_group.name
+  db_subnet_group_name    =  aws_docdb_subnet_group.docdb_subnet_group.name
   vpc_security_group_ids  =  [aws_security_group.allow_mongodb.id]
 }
 
