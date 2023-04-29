05505e3d54005682aaa6a98b47757aa8c3ebe831
amankumar1988 <kumar.aman6600@gmail.com>
2023-04-29 22:09:28 +0530

    Auto Push

--- docdb.tf
+++ docdb.tf
@@ -16,7 +16,7 @@ resource "aws_docdb_subnet_group" "docdb_subnet_group" {
   name       = "robot-${var.ENV}-docdb_subnet_group"
   subnet_ids = data.terraform_remote_state.vpc.outputs.PRIVATE_SUBNET_IDS
   tags = {
-    Name = "robot-${var.ENV}-dodb-subnet-group"
+    Name = "robot-${var.ENV}-docdb-subnet-group"
   }
 }
 
